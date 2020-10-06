b60f2df4a1b70f93913a9653ac487a16902fb05b
rafa_office <central.salas@gmail.com>
2020-10-06 11:28:53 -0500

    Agregada template folder

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Casinos'
+    return render_template('hello.html')
