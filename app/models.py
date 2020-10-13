23e3864236cc2ba005054ccb2ffb8afef7668160
rafa_office <central.salas@gmail.com>
2020-10-13 18:08:49 -0500

    Configuracion exitosa de base de datos e inclusion de informacion

--- app/models.py
+++ app/models.py
@@ -1,12 +1,37 @@
 # Models database
 from flask_sqlalchemy import SQLAlchemy
-from models import app
+from .config import app
+
+import datetime
 
 db = SQLAlchemy(app)
 
-class Personas(db.Model):
-    __tablename__ = 'personas'
+class Clain(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
-    apellido = db.Column(db.String(100), nullable=True)
+    name = db.Column(db.String(100), nullable=False)
+    type_doc = db.Column(db.String(3), nullable=True)
+    nro_doc = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    type_claim = db.Column(db.String(6), nullable=False)
+    amount = db.Column(db.Integer)
+    detail = db.Column(db.String(500), nullable=False)
+    answer_id = db.Column(db.Integer, db.ForeignKey('answer.id'))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.now)
+
+    def __repr__(self):
+        return '<Clain %r>' % self.name
+
+class Answer(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    answer_con = db.Column(db.String(500), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.datetime.now)
+    clain_id = db.Column(db.Integer, db.ForeignKey('clain.id'))
+
+    def __repr__(self):
+        return '<Answer %r>' % self.answer_con
--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 # from Flask
-from flask import render_template
-# from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, Flask, request, redirect, url_for
 
-from app.models import db, Personas
 from app.config import app
+from app.models import db, Clain
 
 # from Python
-from datetime import date
-
-# app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db.sqlite3'
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# db = SQLAlchemy(app)
+from datetime import date, datetime
 
 now = (date.today()).strftime('%d-%m-%Y')
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def client_view():
+    if request.method == 'POST':
+        d = datetime.strptime(request.form["date"], "%Y-%m-%dT%H:%M")
+        new_clain = Clain(
+                name=request.form["name"],
+                type_doc=request.form["type_doc"],
+                nro_doc=request.form["document"],
+                email=request.form["contact"],
+                address=request.form["domicilio"],
+                date=d,
+                type_claim=request.form["type_obj"],
+                amount=request.form["amount"],
+                detail=request.form["detail"],
+                )
+        db.session.add(new_clain)
+        db.session.commit()
+
+        return f'Registro exitoso, le responderemos a su correo {request.form["contact"]} en breve'
+
     return render_template('cliente.html', dia=now)
+
 
 db.create_all()
