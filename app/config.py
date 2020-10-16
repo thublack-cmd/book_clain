e9a6893821aae0a41c789a360b2cffd6e506b450
rafa_office <central.salas@gmail.com>
2020-10-16 18:30:42 -0500

    Se agrego vista de detalle de reclamos atendidos

--- app/config.py
+++ app/config.py
@@ -2,5 +2,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = "b'_5#y2LF4Q8z\n\xec]/"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # from Flask
-from flask import render_template, Flask, request, redirect, url_for
+from flask import render_template, Flask, request, redirect, url_for, flash
 
 from app.config import app
-from app.models import db, Clain
+from app.models import db, Clain, Answer
 
 # from Python
 from datetime import date, datetime
@@ -32,10 +32,31 @@ def client_view():
     return render_template('cliente.html', dia=now)
 
 
-@app.route('/gestion')
+@app.route('/gestion', methods=['GET', 'POST'])
 def audit_view():
+    if request.method == 'POST':
+        new_discharge = Answer(
+                answer_con=request.form['detail_dis']
+                )
+        db.session.add(new_discharge)
+        db.session.commit()
+
+        add_discharge(request.form['id_clain'])
+
+        flash('Descargo guardado exitosamente')
+
+        return redirect(url_for('audit_view'))
+
     q = Clain.query.all()
     return render_template('audit.html', reclamos=q)
+
+def add_discharge(id_clain):
+    # Find last answer ID
+    discharge = Answer.query.order_by(Answer.created_at.desc()).first()
+
+    save_id = Clain.query.get(id_clain)
+    save_id.answer_id = discharge.id
+    db.session.commit()
 
 
 @app.route('/<int:id>/descargo')
@@ -43,9 +64,24 @@ def discharge_view(id):
 
     q = Clain.query.get(id)
 
-    data = {
-            'q': q,
-            }
-    return render_template('discharge.html', **data)
+    if not q.answer_id:
+        data = {
+                'q': q,
+                'id': id,
+                }
+        return render_template('discharge.html', **data)
+
+    flash('Ese reclamo ya fue procesado')
+    return redirect(url_for('audit_view'))
+
+@app.route('/<int:id>/detalle')
+def processed_view(id):
+    q = Clain.query.get(id)
+
+    if not q.answer_id:
+        flash('Este reclamo no ha sido atendido')
+        return redirect(url_for('audit_view'))
+
+    return render_template('detail.html', q=q)
 
 # db.create_all()
