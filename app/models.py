393ec7c1c88dd5a0c6924da648faaeeb23a19abc
rafa_office <central.salas@gmail.com>
2020-10-31 17:54:21 -0500

    Se modifico models y falta aplicar cambios menores

--- app/models.py
+++ app/models.py
@@ -1,7 +1,5 @@
 # Models database
 from flask_sqlalchemy import SQLAlchemy
-# from flask_login import UserMixin
-
 
 import datetime
 
@@ -241,26 +239,3 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.last_name}, {self.first_name}>'
-
-
-# def get_user(user_id):
-#     return User.query.get(user_id)
-
-# class user_data:
-#     def __init__(self, username, password):
-#         self.username = username
-#         self.password = password
-
-# class user_model(UserMixin):
-#     def __init__(self, data_user):
-#         self.id = data_user.username
-#         self.password = data_user.password
-
-#     @staticmethod
-#     def query(user_id):
-#         user = get_user(user_id)
-#         data_user = user_data(
-#                 username = user.username,
-#                 password = user.password,
-#                 )
-#         return user_model(data_user)
