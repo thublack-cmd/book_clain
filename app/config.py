96007855b32e75330abfc7b7669bd6669f55fd40
rafa_office <central.salas@gmail.com>
2020-11-02 18:11:51 -0500

    Cambios menores

--- app/config.py
+++ app/config.py
@@ -1,12 +1,8 @@
-# Configuracion del motor de la base de datos
-# from flask import Flask
-# from flask_mail import Mail
-# from flask_login import LoginManager
 
 class Config:
     SECRET_KEY = "b'_5#y2LF4Q8z\n\xec]/"
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///../db.sqlite3'
-    SQLALCHEMY_DATABASE_URI = 'mysql://rafael:rafaDB.prueba@172.20.20.120/book_clain'
+    SQLALCHEMY_DATABASE_URI = 'mysql://rafael:rafaDB.prueba@172.20.20.231/book_clain'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     USE_SESSION_FOR_NEXT = True
     MAIL_SERVER = 'smtp.gmail.com'
