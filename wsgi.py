ab86f1608bb5c5095693bb51bfcaa37921233cde
rafa_office <central.salas@gmail.com>
2021-07-07 16:57:22 -0500

    updated wsgi

--- wsgi.py
+++ wsgi.py
@@ -4,11 +4,6 @@ from app import create_app
 
 app = create_app()
 
-# @app.login_manager.unauthorized_handler
-# def unauthorized_callback():
-#     flash('Debe ingresar para ver ese contenido')
-#     return redirect('/auth/login')
-
 @app.route('/')
 def index():
     return redirect(url_for('cubatta.audit_view'))
