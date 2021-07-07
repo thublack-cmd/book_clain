54a0af060a2f46ef5613ca2143e33b6314341ce9
rafa_office <central.salas@gmail.com>
2021-07-07 17:49:04 -0500

    Se modifico acceso a base de datos para pruebas de ludopatia

--- app/config.py
+++ app/config.py
@@ -2,9 +2,9 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = "b'_5#y2LF4Q8z\n\xec]/"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://book:pruebaDB@localhost/book_clain'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_BINDS = {
-        'uchukuta': 'mysql+pymysql://book:pruebaDB@localhost/uchu_book_clain'
+        'uchukuta': 'sqlite:///db.sqlite3'
     }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     USE_SESSION_FOR_NEXT = True
