7f1c6a87531d8c3c9813f3932764912c5c960a17
rafa_office <central.salas@gmail.com>
2023-09-01 16:25:03 -0500

    Modificado la pantalla principal y view de publicidad

--- app/publicidad/views.py
+++ app/publicidad/views.py
@@ -15,82 +15,78 @@ def main_view():
 
     if request.method == 'POST':
         for data in request.files.getlist("file"):
-            match data.filename:
-                case "Cubatta.mp4" | "cubatta.mp4":
+            match request.form['sala']:
+                case "Cubatta":
                     ip = '172.20.20.203'
                     userName = 'pi'
                     pword = 'PubliCuba'
                     portN = 9000
 
-                case "Tribeca.mp4":
+                case "Tribeca":
                     ip = '172.20.21.186'
                     userName = 'pi'
                     pword = 'PubliSala'
                     portN = 22
 
-                case "Montreal.mp4":
+                case "Montreal":
                     ip = '172.20.22.184'
                     userName = 'pi'
                     pword = 'PubliSala'
                     portN = 22
 
-                case "Kavari.mp4":
+                case "Kavari":
                     ip = '172.20.23.186'
                     userName = 'pi'
                     pword = 'PubliSala'
                     portN = 22
 
-                case "Siete.mp4":
+                case "Siete":
                     ip = '172.20.24.184'
                     userName = 'pi'
                     pword = 'PubliSala'
                     portN = 22
 
-                case "Magia.mp4":
+                case "Magia":
                     ip = '172.20.25.225'
                     userName = 'pi'
                     pword = 'PubliSala'
                     portN = 9000
 
-                case "Cassino.mp4":
+                case "Cassino":
                     ip = '172.20.26.182'
                     userName = 'pi'
                     pword = 'PubliSala'
                     portN = 22
 
-                case "Proceres.mp4" | "proceres.mp4":
+                case "Proceres":
                     ip = '172.20.23.43'
                     userName = 'pi'
                     pword = 'PubliSala'
                     portN = 22
 
-                case "Huacho.mp4" | "huacho.mp4":
+                case "Huacho":
                     ip = '172.20.24.43'
                     userName = 'pi'
                     pword = 'PubliSala'
                     portN = 22
 
-                case "Huarmey.mp4" | "huarmey.mp4":
+                case "Huarmey":
                     ip = '172.20.25.34'
                     userName = 'pi'
                     pword = 'PubliSala'
                     portN = 22
 
-                case "Casma.mp4" | "casma.mp4":
+                case "Casma":
                     ip = '172.20.26.33'
                     userName = 'pi'
                     pword = 'PubliSala'
                     portN = 22
 
-                case _:
-                    flash('El nombre no coindide con ninguna opcion')
-                    break
-
-            send_update(data, ip, userName, pword, portN)
+            send_update(data, ip, userName, pword, portN, request)
 
     return render_template('video.html')
 
-def send_update(request, ip, userName, pword, portN):
+def send_update(request, ip, userName, pword, portN, local):
     ssh = SSHClient()
     ssh.load_system_host_keys()
 
@@ -99,13 +95,14 @@ def send_update(request, ip, userName, pword, portN):
     with tempfile.TemporaryDirectory() as tmpdir:
         temp_file = Path(tmpdir) / request.filename
         request.save(temp_file)
-        scp.put(temp_file, remote_path='/home/pi/')
+        remote_path = '/home/pi/' + local.form['sala'] + '.mp4'
+        scp.put(temp_file, remote_path)
 
         scp.close()
-        if request.filename == 'Montreal.mp4':
+        if local.form['sala'] == 'Montreal':
             ssh.exec_command("sshpass -p 'PubliSala' scp Montreal.mp4 pi@172.20.22.185:~/")
         # ssh.exec_command("sudo reboot")
         ssh.close()
 
-    flash(f'Se ha actualizado {request.filename}')
+    flash(f'Se ha actualizado {local.form["sala"]}')
     return redirect(url_for('publicidad.main_view'))
